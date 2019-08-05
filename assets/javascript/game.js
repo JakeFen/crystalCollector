db123c4bc4e54202994e98fabc8881bd61c4ecc0
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-08-05 19:29:31 -0400

    gems have random numbers and when clicked they add together to create totalScore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,56 @@
 $(document).ready(function() {
-    var wins = 0;
-    var losses = 0;
-    var totalScore = 0;
-    var gem = "";
+  var wins = 0;
+  var losses = 0;
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+  var totalScore = 0;
 
-    // Creates Random Number and plugs it in
-    $("#random-num").text(Math.floor(Math.random() * 101) + 19);
+  function reset() {
+//   total score of gems
+  totalScore = 0;
+  $("#total-score").text(totalScore);
+  console.log(totalScore);
 
-    // displays starting scores
-    $('#wins').text(wins);
-    $('#losses').text(losses);
-    $('#total-score').text(totalScore);
+  var randomNum = Math.floor(Math.random() * 101) + 19;
+  $("#random-num").text(randomNum);
 
-    // Random numbers for each gem
-    sapphire = (Math.floor(Math.random() * 12) +1);
-    ruby = (Math.floor(Math.random() * 12) + 1);
-    emerald = (Math.floor(Math.random() * 12) + 1);
-    topaz = (Math.floor(Math.random() * 12) + 1);
+//   random num for each gem
+  var sapphire = Math.floor(Math.random() * 12) + 1;
+  console.log(sapphire)
 
+  var ruby = Math.floor(Math.random() * 12) + 1;
+  console.log(ruby)
 
-    $(".gem").click(function() {
-        // testing on sapphire
-        gem = $(".gem").val();
-        console.log(gem);
-        if (gem === 'sapphire') {
-            console.log(sapphire);
-        } 
-        else if (gem === 'ruby') {
-            console.log(ruby);
-        } 
-        else if (gem === 'emerald') {
-            console.log(emerald);
-        } 
-        else if (gem === 'topaz') {
-            console.log(topaz);
-        }
-    })
+  var emerald = Math.floor(Math.random() * 12) + 1;
+  console.log(emerald)
 
+  var topaz = Math.floor(Math.random() * 12) + 1;
+  console.log(topaz)
+
+  $(".gem").click(function() {
+    if ($(this).attr("id") === "sapphire") {
+      totalScore += sapphire;
+    } else if ($(this).attr("id") === "ruby") {
+      totalScore += ruby;
+    } else if ($(this).attr("id") === "emerald") {
+      totalScore += emerald;
+    } else if ($(this).attr("id") === "topaz") {
+      totalScore += topaz;
+    }
+    $("#total-score").text(totalScore);
+
+    if (totalScore === randomNum) {
+        wins++;
+        $("#wins").text(wins);
+        alert("You have won!");
+        reset();
+    } else if (totalScore > randomNum) {
+        losses++;
+        $("#losses").text(losses);
+        alert("You have lost ):");
+        reset();
+    }
+  });
+};
+    reset();
 });
