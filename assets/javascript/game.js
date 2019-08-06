3e21766397c34b797b2e8f1ed2a5ff3ff4b043e9
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-08-05 21:02:25 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,40 @@ $(document).ready(function() {
   $("#wins").text(wins);
   $("#losses").text(losses);
   var totalScore = 0;
+  var randomNum = 0;
+  
+  var sapphire = 0;
+  var ruby = 0;
+  var emerald = 0;
+  var topaz = 0;
 
   function reset() {
 //   total score of gems
   totalScore = 0;
   $("#total-score").text(totalScore);
-  console.log(totalScore);
 
-  var randomNum = Math.floor(Math.random() * 101) + 19;
+  randomNum = Math.floor(Math.random() * 101) + 19;
   $("#random-num").text(randomNum);
 
 //   random num for each gem
-  var sapphire = Math.floor(Math.random() * 12) + 1;
-  console.log(sapphire)
-
-  var ruby = Math.floor(Math.random() * 12) + 1;
-  console.log(ruby)
-
-  var emerald = Math.floor(Math.random() * 12) + 1;
-  console.log(emerald)
-
-  var topaz = Math.floor(Math.random() * 12) + 1;
-  console.log(topaz)
+  sapphire = Math.floor(Math.random() * 12) + 1;
+  ruby = Math.floor(Math.random() * 12) + 1;
+  emerald = Math.floor(Math.random() * 12) + 1;
+  topaz = Math.floor(Math.random() * 12) + 1;
+  }
 
   $(".gem").click(function() {
     if ($(this).attr("id") === "sapphire") {
+      console.log(sapphire);
       totalScore += sapphire;
     } else if ($(this).attr("id") === "ruby") {
+      console.log(ruby);
       totalScore += ruby;
     } else if ($(this).attr("id") === "emerald") {
+      console.log(emerald);
       totalScore += emerald;
     } else if ($(this).attr("id") === "topaz") {
+      console.log(topaz);
       totalScore += topaz;
     }
     $("#total-score").text(totalScore);
@@ -51,6 +54,6 @@ $(document).ready(function() {
         reset();
     }
   });
-};
+
     reset();
 });
